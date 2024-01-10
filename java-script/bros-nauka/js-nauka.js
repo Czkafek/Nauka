337429fc94a186cb29c9702551edea367f59bf3c
Czkafek <czkafek@gmail.com>
2024-01-10 20:11:15 +0100

    10.01.2024

--- java-script/bros-nauka/js-nauka.js
+++ java-script/bros-nauka/js-nauka.js
@@ -77,7 +77,10 @@
             document.getElementById('myh1').textContent = `Welcome ${username} To The Game`
         }
 */
+
+
 /* TYPE CONVERSION 
+
     let age = window.prompt("How old are you?");
     age = Number(age); // konwertuje jaki kolwiek typ dany na typ number (aka int z c++`a)
     age+=1;
@@ -93,3 +96,75 @@
     console.log(y, typeof y);
     console.log(z, typeof z);
 */
+
+
+/* RANDOM NUMBER 
+
+    let randomNum = Math.random(); // it gives a  random number beetween 0 and 1
+    console.log(randomNum);
+
+    let secondRandomNum = Math.floor(Math.random() * 6) +1 ; // tym sposobem zmieniamy zasięg funkcji random z od 0 do 1  na  od 1 do 6, a za pomocą Math.floor zaokrąglamy liczbę
+    console.log(secondRandomNum);
+
+    const min = 50;
+    const max = 100;
+    let thirdRandomNum = Math.floor(Math.random() * (max - min)) + min;
+    console.log(thirdRandomNum);
+
+
+    const losuj = document.getElementById('losuj');
+    let min = 10;
+    let max = 30;
+
+    losuj.onclick = function() {
+        let losowyNumer = Math.floor(Math.random() * (max-min)) + min;
+        document.getElementById('losujH1').textContent = losowyNumer;
+}
+*/
+
+/* CHECKED PROPERTY
+
+    const myCheckBox = document.getElementById('MyCheckBox');
+    const Visa = document.getElementById('VisaBtn');
+    const MasterCard = document.getElementById('MasterCardBtn');
+    const PayPal = document.getElementById('PayPalBtn');
+    const submit = document.getElementById('mySubmit');
+    const subResult = document.getElementById('subResult');
+    const paymentResult = document.getElementById('paymentResult');
+
+    submit.onclick = function() {
+
+        if(myCheckBox.checked) {
+            subResult.textContent = `You are a subscriber!`;
+        }
+        else {
+            subResult.textContent = `You are NOT a subscriber!`;
+        }
+
+        if(Visa.checked) {
+            paymentResult.textContent = 'You are paying with Visa';
+        }
+        else if(MasterCard.checked) {
+            paymentResult.textContent = 'You are paying with MasterCard';
+        }
+        else if(PayPal.checked) {
+            paymentResult.textContent = 'You are paying with PayPal';
+        }
+        else {
+            paymentResult.textContent = 'You are not paying with anything';
+        }
+
+    }
+*/
+
+
+/* TERNARY OPERATOR */
+// shortcut to if{} and else{} statements
+// How to use it:
+// condition ? code-if-true : code-if-false
+let age = 18;
+age>=18 ? "You are an adult" : "You are a minor";
+
+let message = age>=18 ? "You are an adult" : "You are a minor";
+
+console.log(message);
