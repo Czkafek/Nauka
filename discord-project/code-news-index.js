f3d97e00688f5007def4d2ab09aa23ba12a2aab4
Czkafek <czkafek@gmail.com>
2024-01-29 08:27:30 +0100

    lol

---
+++ discord-project/code-news-index.js
@@ -0,0 +1,11 @@
+const input1 = document.getElementById('inputOne');
+const input2 = document.getElementById('inputTwo');
+
+function clearInput() {
+    input1.value = '';
+    input2.value = '';
+}
+
+function changePage() {
+    window.location.href = "articul.html"
+}
