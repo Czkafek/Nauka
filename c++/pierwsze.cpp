7cc5f11fedd1a00b48a380cdfc68a720c5b9aa66
Czkafek <czkafek@gmail.com>
2024-03-11 22:10:39 +0100

    na szybko

---
+++ c++/pierwsze.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+    int a;
+    cin >> a;
+    for(int i = 2; i <= a/2; i++) {
+        if(a%i == 0) {
+            cout<<"TAK";
+            return 0;
+        }
+    }
+    cout<<"NIE";
+    return 0;
+}
