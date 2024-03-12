9c869901779f33017402081788c4c0b8f60f42f5
Czkafek <czkafek@gmail.com>
2024-03-12 22:50:41 +0100

    coś

--- c++/pierwsze.cpp
+++ c++/pierwsze.cpp
@@ -4,13 +4,21 @@ using namespace std;
 
 int main() {
     int a;
-    cin >> a;
-    for(int i = 2; i <= a/2; i++) {
-        if(a%i == 0) {
-            cout<<"TAK";
-            return 0;
+    cin>>a;
+    if(a == 0 || a ==1) {
+        cout<<"NIE";
+        return 0;
+    }
+    else {
+        for(int i = 2; i <= a/2; i++) {
+            if(a%i == 0) {
+                cout<<"NIE";
+                return 0;
+            }
         }
     }
-    cout<<"NIE";
+    cout<<"TAK";
     return 0;
 }
+
+
