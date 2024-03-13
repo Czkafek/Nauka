f4cdec00b54f2736c6099200b5bd1134cd6f15de
Czkafek <czkafek@gmail.com>
2024-03-13 16:29:46 +0100

    Skończyłem problem na spoj'u

--- c++/pierwsze.cpp
+++ c++/pierwsze.cpp
@@ -3,21 +3,28 @@
 using namespace std;
 
 int main() {
-    int a;
-    cin>>a;
-    if(a == 0 || a ==1) {
-        cout<<"NIE";
-        return 0;
-    }
-    else {
-        for(int i = 2; i <= a/2; i++) {
-            if(a%i == 0) {
-                cout<<"NIE";
-                return 0;
+    int n, a;
+    cin>>n;
+    for(int j=0; j<n; j++) {
+        bool czyPierwsza = true;
+        cin>>a;
+        if(a == 0 || a ==1) {
+            cout<<"NIE"<<endl;
+        }
+        else {
+            for(int i = 2; i <= a/2; i++) {
+                if(a%i == 0) {
+                    cout<<"NIE"<<endl;
+                    czyPierwsza = false;
+                    break;
+                }
+            }
+            if(czyPierwsza == true){
+                cout<<"TAK"<<endl;
             }
         }
     }
-    cout<<"TAK";
+    
     return 0;
 }
 
